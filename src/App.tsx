f67d96c40bef6ac9d4e61194b9c68c9af9926eb6
Louie <55088241+852389482@users.noreply.github.com>
2022-05-07 21:51:06 +0800

    setting router for login and layout add sass react-router-dom and types packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import './App.scss'
-
+import Layout from './pages/layout'
+import Login from './pages/login'
+// react router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 function App() {
-  return <div className="app">app</div>
+  return (
+    <>
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path="/home" component={Layout}></Route>
+            <Route path="/login" component={Login}></Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  )
 }
 
 export default App
